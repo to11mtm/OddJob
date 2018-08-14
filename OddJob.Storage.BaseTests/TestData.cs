38691f727cdcb43c1eef0c8ee24d8ca6fe749095
Andrew El <laingas@gmail.com>
2018-08-13 21:06:49 -0400

    Fix Codacy Redline Suggestions

--- OddJob.Storage.BaseTests/TestData.cs
+++ OddJob.Storage.BaseTests/TestData.cs
@@ -2,18 +2,18 @@
 {
     public class TestConstants
     {
-        public const string SimpleParam = "simple";
-        public const string OddParam1 = "odd1";
-        public const string OddParam2 = "odd2";
-        public const string NestedOddParam1 = "n1";
-        public const string NestedOddParam2 = "n2";
+        public static readonly string SimpleParam = "simple";
+        public static readonly string OddParam1 = "odd1";
+        public static readonly string OddParam2 = "odd2";
+        public static readonly string NestedOddParam1 = "n1";
+        public static readonly  string NestedOddParam2 = "n2";
     }
 
     public class MockJob
     {
         public void DoThing(string simpleParam, OddParam oddParam)
         {
-
+            //Intentionally empty since this is testing storage.
         }
     }
 
