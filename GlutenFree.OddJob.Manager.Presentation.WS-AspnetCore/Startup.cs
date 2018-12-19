42d2193bb8500f5c18675c91a62c7bbdb6f3afd4
Andrew El <laingas@gmail.com>
2018-12-18 23:07:17 -0500

    Start Moving Websharper to AspnetCore

---
+++ GlutenFree.OddJob.Manager.Presentation.WS-AspnetCore/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebSharper.AspNetCore;
+
+namespace GlutenFree.OddJob.Manager.Presentation.WS_AspnetCore
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSitelet(Server.Main)
+                .AddAuthentication("WebSharper")
+                .AddCookie("WebSharper", options => { });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment()) { app.UseDeveloperExceptionPage(); }
+
+            app.UseAuthentication()
+                .UseStaticFiles()
+                .UseWebSharper()
+                .Run(context =>
+                {
+                    context.Response.StatusCode = 404;
+                    return context.Response.WriteAsync("Page not found");
+                });
+        }
+
+        public static void Main(string[] args)
+        {
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build()
+                .Run();
+        }
+    }
+}
