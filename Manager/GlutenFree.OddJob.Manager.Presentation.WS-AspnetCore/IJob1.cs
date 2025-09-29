1df235a2ef8db88e1520124f8d8c1f9069f4fc16
to11mtm <12536917+to11mtm@users.noreply.github.com>
2025-09-29 17:11:05 -0400

    Goodbye websharper I will miss you ;_;

--- Manager/GlutenFree.OddJob.Manager.Presentation.WS-AspnetCore/IJob1.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GlutenFree.OddJob.Manager.Presentation.WS_AspnetCore
-{
-    public interface IJob1
-    {
-        void Job1Method1();
-        void Job1Method2();
-    }
-}
--- Manager/GlutenFree.OddJob.Manager.Presentation.WS-AspnetCore/IJob2.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GlutenFree.OddJob.Manager.Presentation.WS_AspnetCore
-{
-    public interface IJob2
-    {
-        void Job2Method1();
-        void Job2Method2(string param1);
-    }
-}
